a40cd53fa996f1ceff32dfbf9cd43fd2b8ce089a
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2025-07-22 19:01:01 +0900

    build: add Spring framework

--- simple-app/src/main/java/com/example/App.java
+++ simple-app/src/main/java/com/example/App.java
@@ -1,11 +1,13 @@
 package com.example;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+@Configuration
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- simple-app/src/test/java/com/example/AppTest.java
+++ simple-app/src/test/java/com/example/AppTest.java
@@ -1,11 +1,17 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = App.class)
 public class AppTest {
     @Test
     public void testApp() {
